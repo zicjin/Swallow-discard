5b8e273ab88849cbc7a97f4b6eb2f78f7e75c5d8
cheney <zicjin@gmail.com>
2016-03-04 12:53:02 +0800

    ssh update

--- shell/aws.sh
+++ shell/aws.sh
@@ -1,1 +1,1 @@
-ssh -i "zicjin.pem" ubuntu@ec2-54-238-211-187.ap-northeast-1.compute.amazonaws.com
+chmod 400 zicjin.pem & ssh -i "zicjin.pem" ubuntu@ec2-54-238-211-187.ap-northeast-1.compute.amazonaws.com
